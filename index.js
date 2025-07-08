75c0894cd0d6fc975934b203f9ee1c8b021c3f9e
Pavel Řežábek <pavelrezabek@Pavels-MacBook-Air.local>
2025-07-08 08:59:03 +0200

    server added

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = 3000;
+
+// Serve invoicer.html on /invoicer
+app.get("/invoicer", (req, res) => {
+  res.sendFile(path.join(__dirname, "invoicer.html"));
+});
+
+// Start server
+app.listen(port, () => {
+  console.log(`Server běží na http://localhost:${port}/invoicer`);
+});
